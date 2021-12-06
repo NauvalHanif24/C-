028d4eb121f6ed2aa02f28d2f146310561f30dc7
Nauval Hanif <94780019+NauvalHanif24@users.noreply.github.com>
2021-12-06 13:01:52 +0700

    Add files via upload

---
+++ hackkerrank2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a;
+	long b;
+	char c;
+	float d;
+	double e;
+	
+	scanf("%d %ld %c %f %lf", &a, &b, &c, &d, &e);
+	
+	printf("%d %ld %c %f %lf",a,b,c,d,e);
+	
+	/*
+	cout << "a: ";
+	cin >> a;
+	cout << "b: ";
+	cin >> b;
+	cout << "c: ";
+	cin >> c;
+	cout << "d: ";
+	cin >> d;
+	cout << "e: ";
+	cin >> e;
+	cout << endl;
+	
+	cout << "a" << a << endl;
+	cout << "b" << b << endl;
+	cout << "c" << c << endl;
+	cout << "d" << d << endl;
+	cout << "e" << e << endl;
+	cout << endl;*/
+	
+	return 0;
+	
+	
+	
+
+}
