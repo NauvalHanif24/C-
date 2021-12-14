752059745b8dae6b11293170da77bb644d8654b2
Nauval Hanif <94780019+NauvalHanif24@users.noreply.github.com>
2021-12-14 11:21:51 +0700

    Add files via upload

---
+++ hackerank3.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string);
+string rtrim(const string);
+
+int main(){
+	
+	string a;
+	getline(cin, a);
+	
+	int n =stoi (ltrim)rtrim(a));
+
+	
+	if(1 <= n <=9){
+		if (n < 9);
+	}
+}
